a4cf79661d21c006260479d53977503f4b2ff583
Brandon Espinosa <espinbrandon49@gmail.com>
2022-11-01 15:00:47 -0700

    deploy

--- server/server.js
+++ server/server.js
@@ -21,4 +21,3 @@ sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Server listening on: http://localhost:${PORT}`));
   // app.listen(process.env.PORT || PORT, () => console.log(`Server listening on: http://localhost:${PORT}`));
 })
-
