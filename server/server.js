4dbe3576996bb3ba5ef892b13ebd02045c11f5d9
Brandon Espinosa <espinbrandon49@gmail.com>
2022-11-01 14:43:44 -0700

    deploy

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
-const cors = require('cors')
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
